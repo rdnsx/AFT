5bf4c5d0bf83a7403bb159d67816b4afcc8515d5
Malte Gebremski <m.gebremski@steinberg.de>
2023-04-08 18:43:53 +0200

    2. iteration

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,7 @@
 FROM python:3.9-slim-buster
-
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends \
-        build-essential \
-        python3-dev \
-        libpq-dev \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
-
 WORKDIR /app
-
-COPY requirements.txt /app
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-
-COPY . /app
-
+COPY . .
 CMD ["python", "aft.py"]
+
--- aft.py
+++ aft.py
@@ -1,57 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import http.server
-import socketserver
-import threading
+from flask import Flask, render_template
 
-# define the name of the CSV file and the directory to serve
-csv_file = '/app/aft.csv'
-static_dir = 'static'
+# read data from CSV file
+df = pd.read_csv('aft.csv')
 
-# create an empty dataframe to store the fertilizer dose or load the existing one
-try:
-    df = pd.read_csv(csv_file)
-except FileNotFoundError:
-    df = pd.DataFrame(columns=['date', 'eisen', 'phosphat', 'nitrat'])
+# create a Flask app
+app = Flask(__name__)
 
-# define the request handler for the web server
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            # show the HTML file that displays the CSV data
-            self.path = '/index.html'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+# define a route for the web page
+@app.route('/')
+def index():
+    # render the template with the data from the CSV file
+    return render_template('index.html', data=df.to_html())
 
-# define a function to start the web server in a separate thread
-def start_web_server():
-    with socketserver.TCPServer(("", 8000), MyHttpRequestHandler) as httpd:
-        print('Web server started at http://localhost:8000')
-        httpd.serve_forever()
-
-# start the web server in a separate thread
-threading.Thread(target=start_web_server).start()
-
-while True:
-    # get the date and fertilizer dose from the user
-    date = input('Enter the date (DD-MM-YYYY): ')
-    eisen = float(input('Enter the Fe dose(drops): '))
-    phosphat = float(input('Enter the PO4 dose (ml): '))
-    nitrat = float(input('Enter the NO3 dose (ml): '))
-    
-    # append the data to the dataframe
-    df = pd.concat([df, pd.DataFrame({'date': date, 'eisen': eisen, 'phosphat': phosphat, 'nitrat': nitrat}, index=[0])], ignore_index=True)
-    
-    # ask the user if they want to continue entering data
-    choice = input('Do you want to enter another dose (y/n)? ')
-    if choice.lower() == 'n':
-        break
-
-# plot the graph
-df.plot(x='date', y=['eisen', 'phosphat', 'nitrat'])
-plt.title('Daily Aquarium Fertilizer Dose')
-plt.xlabel('Date')
-plt.ylabel('Fertilizer Dose')
-plt.savefig(f"{static_dir}/plot.png")
-
-# save the data to the CSV file
-df.to_csv(csv_file, mode='a', index=False, header=not df.index.any())
+if __name__ == '__main__':
+    # run the app on port 8000
+    app.run(debug=True, host='0.0.0.0', port=8000)
