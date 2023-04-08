022ccd3581b2d6bc83361cd6aea0ab35a334ec20
Malte Gebremski <m.gebremski@steinberg.de>
2023-04-08 12:45:07 +0200

    undo

--- aft.py
+++ aft.py
@@ -11,14 +11,13 @@ else:
     df = pd.DataFrame(columns=['date', 'eisen', 'phosphat', 'nitrat'])
 
 # Get user input
-date = input('Enter the date (DD-MM-YYYY): ')
-eisen = float(input('Enter the Fe dose: '))
-phosphat = float(input('Enter the PO4 dose: '))
-nitrat = float(input('Enter the NO3 dose: '))
+date = input('Enter the date (DD.MM.YYYY): ')
+eisen = input('Enter the Fe dose (drops): ')
+phosphat = input('Enter the Phosphat dose (ml): ')
+nitrat = input('Enter the Nitrat dose (ml): ')
 
 # Add input to DataFrame
-df = pd.concat([df, pd.DataFrame({'date': date, 'eisen': eisen, 'phosphat': phosphat, 'nitrat': nitrat}, index=[0])], ignore_index=True)
+df = df.append({'date': date, 'eisen': eisen, 'phosphat': phosphat, 'nitrat': nitrat}, ignore_index=True)
 
 # Write DataFrame to CSV file
 df.to_csv(csv_file, index=False)
-
