630a2230d1f6182e4a04f8993d0873973a4b5db9
Malte Gebremski <m.gebremski@steinberg.de>
2023-04-08 21:55:11 +0200

    Change csv save mode to w

--- aft.py
+++ aft.py
@@ -10,27 +10,39 @@ try:
 except FileNotFoundError:
     df = pd.DataFrame(columns=['date', 'eisen', 'phosphat', 'nitrat'])
 
+# set the 'date' column as the index
+df = df.set_index('date')
+
 while True:
     # get the date and fertilizer dose from the user
     date = input('Enter the date (DD.MM.YYYY): ')
+
+    # check if the date already exists in the index
+    if date in df.index:
+        print('Data for this date already exists. Please enter a different date.')
+        continue
+
     eisen = float(input('Enter the Fe dose(drops): '))
     phosphat = float(input('Enter the PO4 dose (ml): '))
     nitrat = float(input('Enter the NO3 dose (ml): '))
-    
+
     # append the data to the dataframe
-    df = pd.concat([df, pd.DataFrame({'date': date, 'eisen': eisen, 'phosphat': phosphat, 'nitrat': nitrat}, index=[0])], ignore_index=True)
-    
+    df.loc[date] = [eisen, phosphat, nitrat]
+
     # ask the user if they want to continue entering data
     choice = input('Do you want to enter another dose (y/n)? ')
     if choice.lower() == 'n':
         break
 
 # plot the graph
-df.plot(x='date', y=['eisen', 'phosphat', 'nitrat'])
+df.plot()
 plt.title('Daily Aquarium Fertilizer Dose')
 plt.xlabel('Date')
 plt.ylabel('Fertilizer Dose')
 plt.show()
 
+# reset the index to the default integer index
+df = df.reset_index()
+
 # save the data to the CSV file
-df.to_csv(csv_file, mode='a', index=False, header=not df.index.any())
+df.to_csv(csv_file, mode='w', index=False, header=True)
