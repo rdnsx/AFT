a9f59cb6b1ccb70363cc13880aad60a89214aa31
rdnsx <42463214+rdnsx@users.noreply.github.com>
2023-04-08 17:01:04 +0200

    Revert "Beta"

--- aft.py
+++ aft.py
@@ -1,35 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import http.server
-import socketserver
-import threading
 
-# define the name of the CSV file and the directory to serve
-csv_file = '/app/aft.csv'
-static_dir = 'static'
+# define the name of the CSV file
+csv_file = 'aft.csv'
 
 # create an empty dataframe to store the fertilizer dose or load the existing one
 try:
     df = pd.read_csv(csv_file)
 except FileNotFoundError:
     df = pd.DataFrame(columns=['date', 'eisen', 'phosphat', 'nitrat'])
-
-# define the request handler for the web server
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            # show the HTML file that displays the CSV data
-            self.path = '/index.html'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
-
-# define a function to start the web server in a separate thread
-def start_web_server():
-    with socketserver.TCPServer(("", 8000), MyHttpRequestHandler) as httpd:
-        print('Web server started at http://localhost:8000')
-        httpd.serve_forever()
-
-# start the web server in a separate thread
-threading.Thread(target=start_web_server).start()
 
 while True:
     # get the date and fertilizer dose from the user
@@ -51,7 +30,7 @@ df.plot(x='date', y=['eisen', 'phosphat', 'nitrat'])
 plt.title('Daily Aquarium Fertilizer Dose')
 plt.xlabel('Date')
 plt.ylabel('Fertilizer Dose')
-plt.savefig(f"{static_dir}/plot.png")
+plt.show()
 
 # save the data to the CSV file
 df.to_csv(csv_file, mode='a', index=False, header=not df.index.any())
