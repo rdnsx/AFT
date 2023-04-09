9bd7b800126646f29027b542b71abec69bb6eec0
Malte Gebremski <m.gebremski@steinberg.de>
2023-04-09 08:09:18 +0200

    Change column naming

--- aft.py
+++ aft.py
@@ -8,7 +8,7 @@ csv_file = 'aft.csv'
 try:
     df = pd.read_csv(csv_file)
 except FileNotFoundError:
-    df = pd.DataFrame(columns=['Date', 'Fe drops', 'PO4 ml', 'NO3 ml'])
+    df = pd.DataFrame(columns=['Date', 'Fe in drops', 'PO4 in ml', 'NO3 in ml'])
 
 # set the 'Date' column as the index
 df = df.set_index('Date')
